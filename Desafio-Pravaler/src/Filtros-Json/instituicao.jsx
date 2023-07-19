cacbde79c22951c9b2aac579fb7d4568677ee94b
talita <talitamsx@gmail.com>
2023-07-19 20:36:59 -0300

    criação do request api

---
+++ Desafio-Pravaler/src/Filtros-Json/instituicao.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListarDados from '../Api/useDados'
+
+const Instituicao = () => {
+  const instituicao = ListarDados();
+  console.log(instituicao)
+
+  return (
+    <div>
+      <h1> FACULDADE </h1>
+    </div>
+  );
+}
+
+export default Instituicao;
