b69a465d265d3383f15a27edcf115086444d5a48
Ariane <arianevieira26@gmail.com>
2023-07-19 15:11:15 -0300

    boleirplate

---
+++ Desafio-Pravaler/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Página inicial
+      </p>
+    </>
+  )
+}
+
+export default App
